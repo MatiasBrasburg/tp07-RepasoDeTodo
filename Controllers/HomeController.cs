08b28e91bfdd54135d6cfe72e2fb98b8218e3e21
FRANCO URQUIZO BARBA <49257286@ort>
2025-08-07 08:33:02 -0300

    ProyectoCreado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Tp07RepasoTodo_Urquizo_Brasburg.Models;
+
+namespace Tp07RepasoTodo_Urquizo_Brasburg.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp07RepasoTodo_Urquizo_Brasburg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
